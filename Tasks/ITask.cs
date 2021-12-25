3adb0cd81b20aa1872b81d869d81c14f9ca3ad96
Pi-Bouf <pierre.bouffier05@gmail.com>
2021-12-25 17:08:41 +0100

    Add Tasks skeleton

---
+++ Tasks/ITask.cs
@@ -0,0 +1,14 @@
+namespace Wsl_Dev_Manager.Tasks;
+
+public interface ITask
+{
+    public string[] getTaskName();
+
+    public string[] getDependencies();
+
+    public void execute();
+
+    public void logError();
+
+    public void logInfo();
+}
